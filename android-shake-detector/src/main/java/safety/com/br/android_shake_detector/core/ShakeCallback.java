1c74f749f007c305db841f3b8c9c46f0a4c95e57
pecanoski <100592875+pecanoski@users.noreply.github.com>
2024-02-16 16:43:16 +0100

    Added project files

---
+++ android-shake-detector/src/main/java/safety/com/br/android_shake_detector/core/ShakeCallback.java
@@ -0,0 +1,12 @@
+package safety.com.br.android_shake_detector.core;
+
+/**
+ * @author netodevel
+ */
+public interface ShakeCallback {
+
+    void onShake();
+
+    void onData(String err);
+
+}
---
+++ app/src/main/java/safety/com/br/shake_sample/MainActivity.java
@@ -0,0 +1,67 @@
+package safety.com.br.shake_sample;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import safety.com.br.android_shake_detector.core.ShakeCallback;
+import safety.com.br.android_shake_detector.core.ShakeDetector;
+import safety.com.br.android_shake_detector.core.ShakeOptions;
+
+/**
+ * @author netodevel
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private ShakeDetector shakeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buildView();
+
+        ShakeOptions options = new ShakeOptions()
+                .background(false)
+                .interval(1000)
+                .shakeCount(2)
+                .sensibility(1.15f);
+
+        this.shakeDetector = new ShakeDetector(options).start(this, new ShakeCallback() {
+            @Override
+            public void onShake() {
+                Log.d("event", "onShake");
+            }
+        });
+
+        //IF YOU WANT JUST IN BACKGROUND
+        //this.shakeDetector = new ShakeDetector(options).start(this);
+    }
+
+    private void buildView() {
+        Button btnStopService = (Button) findViewById(R.id.btnStopService);
+        btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("destroy", "destroy service shake");
+                shakeDetector.stopShakeDetector(getBaseContext());
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        shakeDetector.destroy(getBaseContext());
+        super.onDestroy();
+    }
+
+}
